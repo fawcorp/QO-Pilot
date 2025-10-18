3dbe51d130a07f78e9d113ae70309442c61a82a2
fawcorp <m.rizki24@gmail.com>
2025-10-18 21:25:52 +0700

    app.py

--- app.py
+++ app.py
@@ -1,31 +1,4 @@
-from document_processor import DocumentProcessor
-
-# Initialize the document processor
-processor = DocumentProcessor()
-
-# Process a document
-file_path = "data/a_scandal_in_bohemia.pdf"
-processor.process_document(file_path)
-
-# Define a query
-query = "What is the main mystery in the story?"
-
-# Verify the document was processed correctly
-print("Test 1: Retrieving context before reset")
-relevant_docs = processor.retrieve_relevant_context(query)
-if relevant_docs:
-    print(f"First chunk preview: {relevant_docs[0].page_content[:100]}...")
-else:
-    print("No relevant documents found.")
-
-# TODO: Call the method to clear the vector store
-processor.reset()
-
-print()
-
-# TODO: Verify that no documents are returned after reset
-relevant_docs = processor.retrieve_relevant_context(query)
-if relevant_docs:
-    print("Document reset not success")
-else:
-    print("No relevant documents found after reset")
+numberfirst=input("Input your number first number please:")
+numbersecond=input("Input your number first number please:")
+sum=numberfirst+numbersecond
+print(numberfirst,"+",numbersecond,"=",sum)
