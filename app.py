1f238e10560d06c1b0f1c38304ba2e849cca65ad
fawcorp <m.rizki24@gmail.com>
2025-10-18 21:47:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-numberfirst=input("Input your number first number please:")
-numbersecond=input("Input your number first number please:")
+import streamlit as st
+
+numberfirst=st.input("Input your number first number please:")
+numbersecond=st.input("Input your number first number please:")
 sum=numberfirst+numbersecond
-print(numberfirst,"+",numbersecond,"=",sum)
+st.print(numberfirst,"+",numbersecond,"=",sum)
+
